280cb9ef5aa424c9194a2d6cfe8a6680f3db5cb2
TheVee7 <vs8273852624@gmail.com>
2023-12-23 00:03:17 +0530

    added str

---
+++ Linked_list.py
@@ -0,0 +1,19 @@
+class node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+class linked:
+    def __init__(self):
+        self.head = None
+        self.n = 0 
+
+    def __str__(self):
+        for i in range(self.n):
+            return self.data
+        
+# a = node(1)    
+# b = node(2)    
+# c = node(3)
+# print(a,b,c)
+
+
